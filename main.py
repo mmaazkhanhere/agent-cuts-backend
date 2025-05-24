9db9bb45a3dfba43e62f53ea82d9d2a61fba03a8
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-05-24 14:53:43 +0500

    demo agent created  and tested.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from demo_agent import weather_time_agent_runner
 
 app: FastAPI = FastAPI()
 
 @app.get("/")
 def get_root():
     return {"Backend is running"}
+
+@app.post('/chat')
+async def get_agent_response(prompt: str):
+    response = await weather_time_agent_runner(prompt)
+    return response
