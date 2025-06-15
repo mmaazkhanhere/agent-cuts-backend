1a82e52690d67827f0b861b7b4080714f30458b3
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-06-15 14:40:55 +0500

    main agent created

---
+++ agent-cuts/agent.py
@@ -0,0 +1,19 @@
+from google.adk.agents import SequentialAgent
+
+
+from .sub_agents.transcription_agent import transcription_agent
+from .sub_agents.segmentation_agent import segmentation_agent
+from .sub_agents.ranking_agent import ranking_agent
+from .sub_agents.video_segmentation_agent import video_segmentation_agent
+
+
+agent_cut = SequentialAgent(
+    name="agent_cut",
+    description="Agent that cuts a video into segments based on topics and rearrange them based on its potential to be viral and trending",
+    sub_agents=[
+        transcription_agent,
+        segmentation_agent,
+        ranking_agent,
+        video_segmentation_agent,
+    ]
+)
