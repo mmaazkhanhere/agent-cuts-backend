83c9fc68ce1ef21b4ff346c0737340f5bd8818a5
M-Abdullah03 <m18ab03@gmail.com>
2025-06-23 15:53:39 +0500

    Switched models

--- agent_cuts/sub_agents/video_segmentation_agent/agent.py
+++ agent_cuts/sub_agents/video_segmentation_agent/agent.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 video_segmentation_agent = Agent(
     name="video_segmentation_agent",
-    model="gemini-2.5-flash",
+    model="gemini-2.0-flash",
     description="Video segmentation agent that segments video based on start and endtime specified in the json input",
     instruction=VIDEO_SEGMENTATION_AGENT_PROMPT,
     tools=[video_segmentation_tool],
